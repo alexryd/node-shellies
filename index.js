18605ed3e8bf1e2b715722c314b4dfa8468c6369
Alexander Ryden <alexander.ryden@gmail.com>
2018-12-14 23:08:33 +0100

    Turn the DeviceManager class into the root object

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
+const EventEmitter = require('events')
+
 const Coap = require('./lib/coap')
-const DeviceManager = require('./lib/device-manager')
+const Device = require('./lib/device')
 const request = require('./lib/http-request')
 const StatusUpdatesListener = require('./lib/status-updates-listener')
 
-module.exports = {
-  Coap,
-  DeviceManager,
-  request,
-  StatusUpdatesListener,
+class Shellies extends EventEmitter {
+  constructor() {
+    super()
+
+    this._devices = new Map()
+    this._listener = new StatusUpdatesListener()
+
+    this._listener.on('statusUpdate', this._statusUpdateHandler.bind(this))
+  }
+
+  async _statusUpdateHandler(msg) {
+    const key = `${msg.deviceType}#${msg.deviceId}`
+    const device = this._devices.get(key)
+
+    if (device) {
+      device.update(msg)
+    } else {
+      let device = null
+
+      try {
+        device = await Device.createFromStatusMessage(msg)
+      } catch (e) {
+        this.emit('error', e)
+        return
+      }
+
+      if (device) {
+        this._devices.set(key, device)
+        this.emit('device', device)
+      } else {
+        // silently ignore unknown devices
+      }
+    }
+  }
+
+  async start() {
+    await this._listener.start()
+  }
+
+  stop() {
+    this._listener.stop()
+  }
+
+  removeDevice(device) {
+    this._devices.delete(`${device.type}#${device.id}`)
+  }
 }
+
+const shellies = new Shellies()
+shellies.Coap = Coap
+shellies.request = request
+shellies.StatusUpdatesListener = StatusUpdatesListener
+
+module.exports = shellies
