bff37cfb42a9e67b554991a9d9ed67aee67972a8
Alexander Ryden <alexander.ryden@gmail.com>
2018-12-14 21:46:59 +0100

    Add support for keeping track of devices and their properties

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Coap = require('./lib/coap')
+const DeviceManager = require('./lib/device-manager')
 const request = require('./lib/http-request')
 const StatusUpdatesListener = require('./lib/status-updates-listener')
 
 module.exports = {
   Coap,
+  DeviceManager,
   request,
   StatusUpdatesListener,
 }
