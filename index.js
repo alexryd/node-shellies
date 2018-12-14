045d5493fe38aca9f86077b1afd64d2f962978fe
Alexander Ryden <alexander.ryden@gmail.com>
2018-12-14 21:44:58 +0100

    Add support for making HTTP requests

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Coap = require('./lib/coap')
+const request = require('./lib/http-request')
 const StatusUpdatesListener = require('./lib/status-updates-listener')
 
 module.exports = {
   Coap,
+  request,
   StatusUpdatesListener,
 }
