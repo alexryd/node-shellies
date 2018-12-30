81a0f594865a7321ba60b7db991e9df532d0883f
Alexander Ryden <alexander.ryden@gmail.com>
2018-12-30 20:14:24 +0100

    Add more tests for the Shellies class

--- test/test-shellies.js
+++ test/test-shellies.js
@@ -9,6 +9,135 @@ describe('shellies', function() {
 
   beforeEach(function() {
     shellies._devices.clear()
+    shellies.removeAllListeners()
+    device.removeAllListeners()
+  })
+
+  it('should emit `start` when the status listener starts', function() {
+    const startHandler = sinon.fake()
+    shellies.on('start', startHandler)
+
+    shellies._listener.emit('start')
+    startHandler.calledOnce.should.equal(true)
+  })
+
+  it('should emit `stop` when the status listener stops', function() {
+    const stopHandler = sinon.fake()
+    shellies.on('stop', stopHandler)
+
+    shellies._listener.emit('stop')
+    stopHandler.calledOnce.should.equal(true)
+  })
+
+  it('should emit `discover` when a new device is found', function() {
+    const discoverHandler = sinon.fake()
+    shellies.on('discover', discoverHandler)
+
+    const msg = {
+      deviceType: 'SHSW-1',
+      deviceId: 'ABC123',
+      host: '192.168.1.2',
+    }
+    shellies._listener.emit('statusUpdate', msg)
+
+    discoverHandler.calledOnce.should.equal(true)
+    discoverHandler.lastCall.args[0].type.should.equal(msg.deviceType)
+    discoverHandler.lastCall.args[0].id.should.equal(msg.deviceId)
+    discoverHandler.lastCall.args[0].host.should.equal(msg.host)
+
+    const msg2 = {
+      deviceType: 'SHSW-1',
+      deviceId: 'ABC124',
+      host: '192.168.1.3',
+    }
+    shellies._listener.emit('statusUpdate', msg2)
+
+    discoverHandler.calledTwice.should.equal(true)
+    discoverHandler.lastCall.args[0].type.should.equal(msg2.deviceType)
+    discoverHandler.lastCall.args[0].id.should.equal(msg2.deviceId)
+    discoverHandler.lastCall.args[0].host.should.equal(msg2.host)
+  })
+
+  it('should not emit `discover` when a known device is found', function() {
+    const discoverHandler = sinon.fake()
+    shellies.on('discover', discoverHandler)
+
+    shellies.addDevice(device)
+
+    const msg = {
+      deviceType: device.type,
+      deviceId: device.id,
+      host: device.host,
+    }
+    shellies._listener.emit('statusUpdate', msg)
+
+    discoverHandler.calledOnce.should.equal(false)
+  })
+
+  it('should emit `add` when a new device is discovered', function() {
+    const addHandler = sinon.fake()
+    shellies.on('add', addHandler)
+
+    const msg = {
+      deviceType: 'SHSW-1',
+      deviceId: 'ABC123',
+      host: '192.168.1.2',
+    }
+    shellies._listener.emit('statusUpdate', msg)
+
+    addHandler.calledOnce.should.equal(true)
+  })
+
+  it('should emit `unknown` when an unknown device type is found', function() {
+    const unknownHandler = sinon.fake()
+    shellies.on('unknown', unknownHandler)
+
+    const msg = {
+      deviceType: 'UNKNOWN-1',
+      deviceId: 'ABC123',
+      host: '192.168.1.2',
+    }
+    shellies._listener.emit('statusUpdate', msg)
+
+    unknownHandler.calledOnce.should.equal(true)
+    unknownHandler.calledWith(
+      msg.deviceType,
+      msg.deviceId,
+      msg.host
+    ).should.equal(true)
+  })
+
+  it('should emit `stale` when a device becomes stale', function() {
+    const clock = sinon.useFakeTimers()
+    const staleHandler = sinon.fake()
+    shellies.on('stale', staleHandler)
+
+    shellies.addDevice(device)
+    device.emit('offline', device)
+
+    staleHandler.called.should.equal(false)
+    clock.tick(shellies.staleTime)
+    staleHandler.calledOnce.should.equal(true)
+    staleHandler.calledWith(device).should.equal(true)
+
+    clock.restore()
+  })
+
+  it('should remove stale devices', function() {
+    const clock = sinon.useFakeTimers()
+    const removeHandler = sinon.fake()
+    shellies.on('remove', removeHandler)
+
+    shellies.addDevice(device)
+    device.emit('offline', device)
+
+    shellies.size.should.equal(1)
+    clock.tick(shellies.staleTime)
+    shellies.size.should.equal(0)
+    removeHandler.calledOnce.should.equal(true)
+    removeHandler.calledWith(device).should.equal(true)
+
+    clock.restore()
   })
 
   describe('#addDevice()', function() {
@@ -126,6 +255,18 @@ describe('shellies', function() {
       iterator.next().value.should.equal(device)
       iterator.next().value.should.equal(device2)
       iterator.next().value.should.equal(device3)
+    })
+  })
+
+  describe('#setAuthCredentials()', function() {
+    it('should set the authentication credentials', function() {
+      const auth = sinon.fake()
+      sinon.replace(shellies.request, 'auth', auth)
+
+      shellies.setAuthCredentials('foo', 'bar')
+
+      auth.calledOnce.should.equal(true)
+      auth.calledWith('foo', 'bar').should.equal(true)
     })
   })
 })
